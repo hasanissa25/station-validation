ba6db72b87da50fe658b81c336866d98c0529510
Hasan Issa <hasan.issa@nrcan-rncan.gc.ca>
2022-06-22 10:21:38 -0400

    WIP: Updated the stationXML

--- stationverification/bin/fetch_station_xml.py
+++ stationverification/bin/fetch_station_xml.py
@@ -2,6 +2,6 @@ import requests
 
 
 def main():
-    station_url = "http://fdsn.seismo.nrcan.gc.ca/fdsnws/station/1/query?network=QW&level=channel&nodata=404"  # noqa
+    station_url = "http://fdsn.seismo.nrcan.gc.ca/fdsnws/station/1/query?network=QW&level=response&nodata=404"  # noqa
     request = requests.get(station_url, allow_redirects=True)
     open('stationverification/data/QW.xml', 'wb').write(request.content)
