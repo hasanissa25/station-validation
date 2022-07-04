d861884925dec1ad20ada43baf2f846df4c37af7
Hasan Issa <hasan.issa@nrcan-rncan.gc.ca>
2022-07-04 09:01:42 -0400

    Added raise for status to handle errors when fetching stationxml

--- stationverification/bin/fetch_station_xml.py
+++ stationverification/bin/fetch_station_xml.py
@@ -4,4 +4,5 @@ import requests
 def main():
     station_url = "http://fdsn.seismo.nrcan.gc.ca/fdsnws/station/1/query?network=QW&level=response&nodata=404"  # noqa
     request = requests.get(station_url, allow_redirects=True)
+    request.raise_for_status()
     open('stationverification/data/QW.xml', 'wb').write(request.content)
